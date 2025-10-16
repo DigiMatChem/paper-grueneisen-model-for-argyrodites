1a7d4810afe6b5c4c14828c6e1f75fd4e4916dd3
joabustamante <joabustamante@gmail.com>
2025-10-16 16:16:38 +0200

    move folders

--- Group_velocity/Ag8GeS6/plot_velocity_freq.py
+++
@@ -1,6 +0,0 @@
-from velocity import VelocityPlotter
-from velocity import get_velocity
-
-vel=get_velocity("mesh.yaml")
-
-VelocityPlotter(vel).save_plot("velocity_Kms-1.png")
--- transport_properties/Grüneisen_parameter/RT/run_gru_mesh.sh
+++
@@ -1,2 +0,0 @@
-phonopy-gruneisen org plus minus --dim="1 3 2" --pa="auto" --mesh="10 19 14" -p -c POSCAR -s --nac 
-
